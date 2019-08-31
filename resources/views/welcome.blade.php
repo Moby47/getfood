3eba0be8117a8c9047b6fcf15041f4a6a43b9cb9
henry <henryonyemaobi@gmail.com>
2019-08-31 10:55:07 +0100

    first commit. complete framwork

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LaraVue</title>
+        <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+<link href="/images/apple-touch-startup-image-320x460.png" media="(device-width: 320px)" rel="apple-touch-startup-image">
+<link href="/images/apple-touch-startup-image-640x920.png" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+      
+<title>GetFoods</title>
 
     <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">   
@@ -16,9 +24,11 @@
 -->
 <!--nprogress-css-->
 <link href="https://unpkg.com/nprogress@0.2.0/nprogress.css" rel="stylesheet" />
-
-
 <link href="/css/custom.css" rel="stylesheet" />
+
+<link rel="stylesheet" href="/css/framework7.css">
+<link rel="stylesheet" href="/css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet"> 
 
  <!--fav icon 
  <link rel="icon" href="{{ asset('images/fav.jpg') }}">
@@ -40,6 +50,7 @@
 
          <script src="https://unpkg.com/nprogress@0.2.0/nprogress.js"></script>
         <script src="{{asset('/js/app.js')}}"></script>
-
+        <script type="text/javascript" src="/js/jquery-1.12.4.min.js"></script>
+        <script type="text/javascript" src="/js/framework7.js"></script>
     </body>
 </html>
