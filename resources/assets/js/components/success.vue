3832d7711e389f3b0a7affc04a77826dcaf35b9f
henry <henryonyemaobi@gmail.com>
2020-01-19 01:03:27 +0100

    repairs menu bar and call to action in success page

--- resources/assets/js/components/success.vue
+++ resources/assets/js/components/success.vue
@@ -1,67 +1,71 @@
 <template>
-    <div class="container">
-        
-        <menubar></menubar>
+  <div class="container">
+    
 <!--content here-->
 
 <div class="pages">
-    <div data-page="success" class="page no-toolbar no-navbar">
-      <div class="page-content">
+  <div data-page="cart" class="page no-toolbar no-navbar">
+    <div class="page-content">
+          <div id="pages_maincontent">
+           
+           <menubar></menubar>
+           <br>
+          
+
+    <div class="page_single layout_fullwidth_padding">	
       
-       <div id="pages_maincontent">
-  
-      <div class="page_single layout_fullwidth_padding bounceIn mt-5">
-              <div class="success_message">
-              <span>Thank You!</span>
-              <img src="/images/icons/black/rocket.png" alt="" title="" />
-              <p>Your Order <br />Has Been successfully completed.</p>   
-              </div>
-        </div>
-        
-        </div>
-        
-        
+         <div class="cartcontainer">   
+      
+          <div class="page_single layout_fullwidth_padding bounceIn mt-5">
+            <div class="success_message">
+            <span>Thank You!</span>
+            <img src="/images/icons/black/rocket.png" alt="" title="" />
+            <p>Your Order <br />Has Been successfully completed.</p>   
+            <p><router-link to='/'>Go Home</router-link></p> 
+            </div>
       </div>
-    </div>
+
+ </div>
   </div>
-  
-  <!--content here-->
+  </div>
+  </div>
+  </div>
+</div>
 
   <back></back> 
-  
-    </div>
+
+  </div>
 </template>
 
 
-          
 <script>
-    export default {
-   //meta
-   metaInfo: {
-       charset: 'utf-8' ,
-       name: 'viewport', content: 'width=device-width, initial-scale=1',
-      title: 'Thanks - GetFoods',
-         meta: [{
-           'name': 'description',
-        'content': 'Online food reservation and ordering platform to maintain a reliable and efficient system',
-      }],
-      noscript: [
-      { innerHTML: 'JavaScript is required.' }
-    ]
-    },
-    //meta
-        data(){
-            return {
-             
-            }
-        },
+  export default {
+ //meta
+ metaInfo: {
+     charset: 'utf-8' ,
+     name: 'viewport', content: 'width=device-width, initial-scale=1',
+    title: 'Thanks - GetFoods',
+       meta: [{
+         'name': 'description',
+      'content': 'Online food reservation and ordering platform to maintain a reliable and efficient system',
+    }],
+    noscript: [
+    { innerHTML: 'JavaScript is required.' }
+  ]
+  },
+  //meta
+      data(){
+          return {
+           
+          }
+      },
 
-        methods: {
+      methods: {
 
-        },
+      },
 
-        mounted() {
-            //need a way to prevent access unless a successful order was made!!!!
-        }
-    }
+      mounted() {
+          //need a way to prevent access unless a successful order was made!!!!
+      }
+  }
 </script>
