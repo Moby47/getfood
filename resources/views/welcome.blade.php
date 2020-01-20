8dc7cedcc60ba27069b455f5f118ca1341ae854c
henry <henryonyemaobi@gmail.com>
2020-01-20 18:47:39 +0100

    fix for apple

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,16 @@
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="GetFood">
-
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-57x57.png" sizes="57x57">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-60x60.png" sizes="60x60">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-72x72.png" sizes="72x72">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-76x76.png" sizes="76x76">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-114x114.png" sizes="114x114">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-120x120.png" sizes="120x120">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-144x144.png" sizes="144x144">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-152x152.png" sizes="152x152">
+  <link rel="apple-touch-icon" href="/images/apple-icons/apple-icon-180x180.png" sizes="180x180">
+  <meta name="msapplication-TileImage" content="/images/app-icons/app-icon-144x144.png">
   <meta name="msapplication-TileColor" content="#fff">
   <meta name="theme-color" content="#ffb42e">
 
