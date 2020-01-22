df75a316dee5a0146b408a4ff8676dc827bc4f8f
henry <henryonyemaobi@gmail.com>
2020-01-22 19:56:42 +0100

    creates rating model and migration

---
+++ database/migrations/2020_01_22_185132_create_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('user_email');
+            $table->integer('vendor_id');
+            $table->string('vendor_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
