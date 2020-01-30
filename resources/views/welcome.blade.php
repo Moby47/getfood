ea8895b82aaca4a7c1aa50124695b90abc57c202
Moby47 <henryonyemaobi@gmail.com>
2020-01-30 08:00:16 +0000

    adds tawk.to chat widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -289,4 +289,17 @@ function clearAllData(table){
 </script>
 
     </body>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e328a218e78b86ed8abc6b5/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </html>
